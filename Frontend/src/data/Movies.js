ae114bc6722ae3640a8e21f2296c4285ffbcfa18
lifeofstephanie <stephanieanyanwu605@gmail.com>
2024-12-12 13:56:44 +0100

    trending tv shows, trending movies and details

---
+++ Frontend/src/data/Movies.js
@@ -0,0 +1,53 @@
+const movies = [
+    {
+        title:'Monsters, INC.',
+        posterUrl: require('../assets/monsters.jpeg'),
+        description:'Animated Film that explores the world of Monstropolis, where monsters generate their city\'s power by scaring children at night'
+    },
+    {
+        title:'Monsters Resort.',
+        posterUrl: require('../assets/landing.jpg'),
+        description:'Animated Film that explores the world of Monstropolis, where monsters generate their city\'s power by scaring children at night'
+    },
+    {
+        title:'Monsters, INC.',
+        posterUrl: require('../assets/learning.jpeg'),
+        description:'Animated Film that explores the world of Monstropolis, where monsters generate their city\'s power by scaring children at night'
+    },
+    {
+        title:'Monsters Resort.',
+        posterUrl: require('../assets/landing.jpg'),
+        description:'Animated Film that explores the world of Monstropolis, where monsters generate their city\'s power by scaring children at night'
+    },
+    {
+        title:'Monsters, INC.',
+        posterUrl: require('../assets/learning.jpeg'),
+        description:'Animated Film that explores the world of Monstropolis, where monsters generate their city\'s power by scaring children at night'
+    },
+    {
+        title:'Monsters Resort.',
+        posterUrl: require('../assets/landing.jpg'),
+        description:'Animated Film that explores the world of Monstropolis, where monsters generate their city\'s power by scaring children at night'
+    },
+    {
+        title:'Monsters, INC.',
+        posterUrl: require('../assets/learning.jpeg'),
+        description:'Animated Film that explores the world of Monstropolis, where monsters generate their city\'s power by scaring children at night'
+    },
+    {
+        title:'Monsters Resort.',
+        posterUrl: require('../assets/landing.jpg'),
+        description:'Animated Film that explores the world of Monstropolis, where monsters generate their city\'s power by scaring children at night'
+    },
+    {
+        title:'Monsters, INC.',
+        posterUrl: require('../assets/learning.jpeg'),
+        description:'Animated Film that explores the world of Monstropolis, where monsters generate their city\'s power by scaring children at night'
+    },
+    {
+        title:'Monsters Resort.',
+        posterUrl: require('../assets/landing.jpg'),
+        description:'Animated Film that explores the world of Monstropolis, where monsters generate their city\'s power by scaring children at night'
+    }
+]
+export default  movies
